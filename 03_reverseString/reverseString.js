c2bc1880a1841f093987306bac902fb64e668d90
jmcbee-dev <mcbee@mines.edu>
2024-02-05 13:23:58 -0700

    Completed Ex.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   let rev = '';
+   for(let i = str.length - 1; i >= 0; i--) 
+      rev += str[i];
+   return rev;
 };
 
 // Do not edit below this line
