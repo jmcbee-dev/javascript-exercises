49844e1963fa47630bc078afe3a697707558b08f
jmcbee-dev <mcbee@mines.edu>
2024-02-05 13:19:40 -0700

    Completed Ex.2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(str, rep) {
+   if(rep < 0)  return "ERROR";
 
+   if(rep == 0) return '';
+   
+   return str + repeatString(str, rep - 1);
 };
 
 // Do not edit below this line
