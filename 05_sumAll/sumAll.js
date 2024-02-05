c6d1c00663b34266519a447bfc31aa408e306bf4
jmcbee-dev <mcbee@mines.edu>
2024-02-05 13:52:26 -0700

    Completed Ex.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+   if(num1 < 0 || num2 < 0 || 
+      typeof(num1) !== 'number' || 
+      typeof(num2) !== 'number' ) return "ERROR";
+   
+   if(num1 > num2) return sumAll(num2, num1);
 
+   let sum = 0;
+   for(; num1 <= num2; num1++) 
+      sum += num1;
+   return sum;
 };
 
 // Do not edit below this line
