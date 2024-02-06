47022c56ae21e577dbbc181fd15de005dfd9249d
jmcbee-dev <mcbee@mines.edu>
2024-02-05 19:48:04 -0700

    Completed Ex.8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {  
+  return numbers.reduce( (pre, current) => pre + current, 0);
 };
 
-const multiply = function() {
 
+const multiply = function(numbers) {
+  return numbers.reduce( (pre, current) => pre * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	return base ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num < 0) return "ERROR";
+	return (num === 0) ? 1 : num*factorial(num - 1);
 };
 
 // Do not edit below this line
