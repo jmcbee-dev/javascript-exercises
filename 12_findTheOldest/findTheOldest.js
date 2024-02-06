6e9190a1cbe43972c8b61341a1d841a81e0a5833
jmcbee-dev <mcbee@mines.edu>
2024-02-05 20:51:44 -0700

    Completed Ex.12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   return people.reduce( (oldest, person) => {
+      const oldestAge = getAge(oldest);
+      const age =  getAge(person);
+      return oldestAge < age ? person : oldest
+   });
+};
 
+const getAge = function(person){
+   return (!person.yearOfDeath ? new Date().getFullYear() : person.yearOfDeath) - person.yearOfBirth;
 };
 
 // Do not edit below this line
