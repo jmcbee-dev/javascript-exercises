f8714593f0901f2b2dfc31044bbb411585682db8
jmcbee-dev <mcbee@mines.edu>
2024-02-05 19:58:45 -0700

    Completed Ex.9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+   for(let i = 0; i < Math.ceil(str.length / 2); i++)
+      if(str[i] != str[str.length - i - 1])
+         return false;
+   return true;
 };
 
 // Do not edit below this line
